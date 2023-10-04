5ba613d048fb25792f18b9ad1ab6ac8a514884f8
cwhitecoding <143142366+cwhitecoding@users.noreply.github.com>
2023-10-04 12:28:56 -0500

    JS updated, files arranged

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,65 @@
-//  start button click
-    // starts the game displaying the first question
-    // starts timer
-    // See time in corner of the screen
+// All variables global to start with
 var timer;
 var timerCount;
 var score;
 var timerElement = document.getElementById("timer-display");
+var questionsEl = document.getElementById("ques");
+var answerEl = document.getElementById("answers");
 var startButton = document.getElementById("startButton");
 
+// Creates an array to grab from to keep displaying questions and answers.
+var questions = [
+    {
+        question: "What is a JavaScript array?",
+        answers: ["A global variable", "An object that contains data", "A property of an object that contains a definition", "A process that is declared to perform an action"],
+        correct: "An object that contains data",
+    },
+    {
+        question: "When logically comparing two values in Javascript, what symbol is used to define the inverse of a value?",
+        answers: ["^","!","#","||"],
+        correct: "!",
+    },
+    {
+        question: "What is the computer language used to style and design a website?",
+        answers: ["Python","JavaScript","CSS","HTML"],
+        correct: "CSS",
+    },
+    {
+        question: "What coding language is most commonly used in creating websites?",
+        answers: ["Python","Jaguar","JavaScript","CoffeeBean"],
+        correct: "JavaScript",
+    },
+    {
+        question: "What part of a plant is used to exchange gasses with the atmosphere?",
+        answers: ["Stomata","Gibberellin","Anthocyanins","Apical Meristem"],
+        correct: "Stomata",
+    },
+];
+
+// This variable is a regular integer that will help select within the array
+var qIndex = 0
+
+// Displays questions and answers continuously after button pushing.
+function displayQ() {
+    var pEl = document.createElement("p");
+    answerEl.innerHTML = "";
+    questionsEl.innerHTML = "";
+    for (var i = 0; i < questions[qIndex].answers.length; i++) {
+        var buttonEl = document.createElement("button");
+        buttonEl.textContent = questions[qIndex].answers[i];
+        answerEl.append(buttonEl);
+    }
+    pEl.textContent = questions[qIndex].question;
+    questionsEl.append(pEl);
+}
+
+// Begins timer function setting to stop at zero.
 function startTimer() {
     timer = setInterval (function() {
         timerCount--;
         timerElement.textContent = timerCount;
         if (timerCount >= 0) {
-            if (score && timerCount > 0) {
+            if (timerCount === 0) {
                 clearInterval(timer);
             }
         }
@@ -24,65 +70,26 @@ function startTimer() {
     }, 1000);
 }
 
+// Gives parameters on what is necessary to start the game.
 function startGame() {
-    timerCount = 30;
-    startButton = true;
-    startTimer()
+    timerCount = 45;
+    startButton.classList.add("hide");
+    startTimer();
+    displayQ();
 }
 
-startButton.addEventListener("click", startGame);
-
-// Multiple Choice Question
-// wrong answer subtracts time from timer
-function wrongAnswer() {
-    timerCount--;
-    if (isCorrect === false) {
-        timerCount += -5;}
+// Function to allow wrong answer to subtract time from timer.
+function wrongAnswer(event) {
+    if (questions[qIndex].correct === event.target.textContent) {
+    console.log("correct");
     }
-
-// answer button click displays next question
-var question1 = "What is a Javascript array?";
-var answer1 = "A global variable";
-var answer12 = "An object that contains data"; //Correct answer
-var answer13 = "A property of an object that contains a definition";
-var answer14 = "A process that is declared to perform an action";
-var question2 = "When logically comparing two values in Javascript, what symbol is used to define the inverse of a value?";
-var answer2 = "!"; //Correct answer
-var answer22 = "^";
-var answer23 = "#";
-var answer24 = "||";
-var question3 = "What is the computer language used to style and design a website?";
-var answer3 = "Python";
-var answer32 = "JavaScript"; 
-var answer33 = "CSS"; //Correct answer
-var answer34 = "HTML";
-var question4 = "What coding language is most commonly used in creating websites?";
-var answer4 = "Python";
-var answer42 = "Jaguar";
-var answer43 = "JavaScript"; //Correct answer
-var answer44 = "CoffeeBean";
-var question5 = "What part of a plant is used to exchange gasses with the atmosphere?";
-var answer5 = "Stomata"; //Correct answer
-var answer52 = "Gibberellin";
-var answer53 = "Anthocyanins";
-var answer54 = "Apical Meristem";
-
-var questions = [question1, question2, question3, question4, question5];
-var answers1 = [answer1, answer12, answer13, answer14];
-var answers2 = [answer2, answer22, answer23, answer24];
-var answers3 = [answer3, answer32, answer33, answer34];
-var answers4 = [answer4, answer42, answer43, answer44];
-var answers5 = [answer5, answer52, answer53, answer54];
-
-for (var i = 0; i < answers1.length; i++) {
-
-}
-
-function loseGame() {
-    if (timerCount === 0) {
-        clearInterval(timer);}
-}
-// When timer reaches 0, or completes all questions, end the game
-    // Once game is over, timer disappears
-    // on game over screen, user can input initials and save score
-    // on game over screen, user can press buanswer14
+    else {
+        console.log("incorrct");
+        timerCount = timerCount-5;
+    }
+    qIndex++
+    displayQ();
+    }
+    
+    startButton.addEventListener("click", startGame);
+    answerEl.addEventListener("click", wrongAnswer);
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-This coding quiz 
+This coding quiz was created using HTML, CSS, and JavaScript. It allows a user to test their knowledge on mostly coding content and one question about plants. The quiz helped me understand functions and methods better in JavaScript.
 
 ## Installation
 
@@ -10,15 +10,21 @@ N/A
 
 ## Usage
 
-Start
+Click start and begin answering questions by clicking buttons. The wrong answer will subtract 5 seconds from the timer. 
+The main page welcomes you and presents the start button:
+![Begin quiz screen](./Assets/images/start-screen.png)
+
+Continue answering questions by clicking buttons.. but don't forget about the last question!
+![Question examples](./Assets/images/question-example.png)
 
 ## Credits
+
+### Eric Sayer: One hour tutor session
+### Brennan Mitchell: 45 minute group study session
 
 ### CSS Reset from: https://meyerweb.com/eric/tools/css/reset/
 
 ## License
 
 N/A
-
-## Tests
 
